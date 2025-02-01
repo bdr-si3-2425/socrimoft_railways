942f4c3125a215edbae12a633849284d5e654af2
Louis DUBAN <louis.duban@etu.unice.fr>
2025-02-01 21:33:05 +0100

    Requêtes Q6 et clear.sql

--- questions/q6/q6data.sql
+++ questions/q6/q6data.sql
@@ -1,1 +1,75 @@
-select * from gare;
+-- Quels trains peuvent être réaffectés pour couvrir une panne sur une autre ligne ?
+INSERT INTO type_train (id_type_train, nom, capacite_max, vitesse_max) VALUES 
+(1, 'TER', 10, 120),
+(2, 'TGV', 15, 370);
+
+INSERT INTO type_gare (id_type_gare, nom) VALUES (1, 'gare voyageur');
+
+-- 4 gares
+INSERT INTO gare (id_gare, nom, adresse, id_type_gare) VALUES 
+(1,'gare1', 'impasse 4 vents',1),
+(2,'gare2', 'rue 3 vents',1),
+(3,'gare3', 'impasse 2 oiseaux',1),
+(4,'gare4', 'rue 1 oiseau',1);
+
+INSERT INTO trajet(id_trajet, gare_depart, gare_arrive) VALUES 
+-- gare 1 -> gare 4
+(1,1,2),
+(2,2,3),
+(3,3,4),
+-- gare 4 -> gare 1
+(4,4,3),
+(5,3,2),
+(6,2,1),
+-- gare 2 -> gare 4
+(7,2,4),
+-- gare 4 -> gare 2
+(8,4,2);
+
+-- Trains 1-4 rattaché à des trajets, trains 5-9 rattaché à des gares
+INSERT INTO train (id_train, heures_fonctionnement, gare_actuelle, id_type_train, trajet_actuel, ligne_habituelle) VALUES 
+(1, 1000, NULL, 1, 1, NULL),
+(2, 1000, NULL, 1, 2, NULL),
+(3, 1000, NULL, 1, 3, NULL),
+(4, 1000, NULL, 2, 4, NULL),
+(5, 1000, 1, 2, NULL, NULL),
+(6, 1000, 2, 2, NULL, NULL),
+(7, 1000, 3, 2, NULL, NULL),
+(8, 1000, 4, 1, NULL, NULL),
+(9,  800, 2, 1, NULL, NULL);
+
+INSERT INTO type_incident (id_type_incident, nom) VALUES 
+(1, 'ampoule grillée'),
+(2, 'roues endommagées'),
+(3, 'déraillage');
+
+-- 3 niveaux de risque différents de 1 à 3
+INSERT INTO niveau_risque (id_niveau_risque, nom, niveau) VALUES 
+(1, 'léger', 1),
+(2, 'moyen', 2),
+(3, 'important', 3);
+
+INSERT INTO incident (id_incident, date_incident, retard, cout, resolu, id_type_incident, id_niveau_risque, id_gare) VALUES 
+(1, '2025-01-27 16:30:12', '00:00:00', 10, false, 1, 1, NULL),
+(2, '2025-01-27 16:32:50', '00:25:00', 10, false, 2, 2, NULL),
+(3, '2025-01-27 16:36:50', '02:15:00', 10, false, 3, 3, NULL),
+(4, '2025-01-28 16:30:12', '00:00:00', 10, false, 1, 1, NULL),
+(5, '2025-01-28 16:32:50', '00:20:00', 10, false, 2, 2, NULL),
+(6, '2025-01-30 16:36:50', '02:45:00', 10, false, 3, 3, NULL),
+(7, '2025-01-27 16:40:50', '00:25:00', 10, false, 2, 2, NULL),
+(8, '2025-01-27 15:27:50', '02:45:00', 10, false, 3, 3, NULL),
+(9, '2025-01-27 15:27:50', '02:45:00', 10,  true, 3, 3, NULL);
+
+-- Différents incidents qui permettent de tester q6view.sql unitairement
+INSERT INTO INCIDENT_TRAIN (id_train, id_incident) VALUES 
+(1, 2), -- Train 1 impacté par un incident niveau 2 non résolu
+(3, 9), -- Train 3 impacté par un incident niveau 3 résolu
+(4, 9), -- Train 4 impacté par un incident niveau 1 non résolu
+(4, 8), -- Train 4 impacté par un incident niveau 3 non résolu
+(6, 8), -- Train 6 impacté par un incident niveau 3 non résolu
+(5, 9); -- Train 5 impacté par un incident niveau 3 résolu
+
+
+
+
+ 
---
+++ questions/q6/q6view.sql
@@ -0,0 +1,43 @@
+-- Louis DUBAN SI3 FISE
+-- 6. Quels trains peuvent être réaffectés pour couvrir une panne sur une autre ligne ?
+WITH Train_en_panne AS (
+    -- Identifier les trains en circulation (ayant un trajet actuel) et étant en panne avec un niveau de risque supérieur à 2.
+    SELECT DISTINCT t.id_train AS id_train_panne, t.trajet_actuel
+    FROM TRAIN t
+    JOIN INCIDENT_TRAIN it ON it.id_train = t.id_train
+    JOIN INCIDENT i ON it.id_incident = i.id_incident
+	JOIN NIVEAU_RISQUE nr ON i.id_niveau_risque = nr.id_niveau_risque
+    WHERE nr.niveau >= 2 AND i.resolu = FALSE
+    AND t.trajet_actuel IS NOT NULL
+),
+Gares_Impactees AS (
+    -- Trouver les gares de départ et d'arrivée des trajets des trains en panne
+    SELECT DISTINCT t.trajet_actuel, tr.gare_depart AS id_gare FROM TRAIN t
+    JOIN TRAJET tr ON t.trajet_actuel = tr.id_trajet
+    WHERE t.id_train IN (SELECT id_train_panne FROM Train_en_panne)
+    UNION
+    SELECT DISTINCT t.trajet_actuel, tr.gare_arrive AS id_gare FROM TRAIN t
+    JOIN TRAJET tr ON t.trajet_actuel = tr.id_trajet
+    WHERE t.id_train IN (SELECT id_train_panne FROM Train_en_panne)
+),
+Trains_Disponibles AS (
+    -- Associer les trains disponibles aux trajets des trains en panne pour s'assurer qu'ils sont bien dans les gares du trajet concerné
+    SELECT t.id_train AS id_train_dispo, t.gare_actuelle, gi.trajet_actuel
+    FROM TRAIN t
+    JOIN Gares_Impactees gi ON t.gare_actuelle = gi.id_gare
+	-- Sous requête
+    WHERE NOT EXISTS (
+        SELECT 1 FROM INCIDENT_TRAIN it
+        JOIN INCIDENT i ON it.id_incident = i.id_incident
+        WHERE it.id_train = t.id_train AND i.resolu = FALSE
+    )
+)
+-- Associer chaque train en panne uniquement aux trains disponibles dans les gares de son propre trajet
+SELECT 
+    tp.id_train_panne, 
+    td.id_train_dispo, 
+    g.nom AS nom_gare
+FROM Train_en_panne tp
+JOIN Trains_Disponibles td ON tp.trajet_actuel = td.trajet_actuel
+JOIN GARE g ON td.gare_actuelle = g.id_gare
+ORDER BY tp.id_train_panne;
