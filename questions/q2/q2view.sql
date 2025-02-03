9c3eaca4e7036cb676c81a6015f6213e76170d00
Baptiste Monsterleet <monsterleetbaptiste@gmail.com>
2025-02-03 17:07:13 +0100

    add comments

--- questions/q2/q2view.sql
+++ questions/q2/q2view.sql
@@ -1,17 +1,22 @@
+-- La vue renvoie la liste de tous les trains qui remplissent au moins l'un des deux conditions :
+-- Le train à obtenu un niveau de risque supérieur ou égal à 3 depuis sa dernière maintenance
+-- Le train à cumulé 3000 heures de trajet ou plus depuis sa dernière maintenance
+-- Si le train n'a jamais eu de maintenance auparavant :
+-- On regarde si ses données actuelles remplissent les conditions précédentes
 CREATE OR REPLACE VIEW train_besoin_maintenance (id_train, heures_depuis_derniere_maintenance, risque_depuis_derniere_maintenance) AS
 SELECT 
-	t.id_train, 
-	(t.heures_fonctionnement-COALESCE(m.heures_fonctionnement,0)) AS heures_depuis_derniere_maintenance, 
-	(SUM(COALESCE(r.niveau,0)) - COALESCE(m.niveau_risque,0)) AS risque_depuis_derniere_maintenance
+t.id_train, 
+-- Coalesce permet de gérer le cas ou le train n'a jamais eu de maintenance
+(t.heures_fonctionnement-COALESCE(m.heures_fonctionnement,0)) AS heures_depuis_derniere_maintenance, 
+-- Le premier Coalesce permet de gérer le cas ou le train n'a jamais eu d'incidents
+-- Le deuxième permet de gérer le cas ou le train n'a jamais eu de maintenance
+(SUM(COALESCE(r.niveau,0)) - COALESCE(m.niveau_risque,0)) AS risque_depuis_derniere_maintenance
 FROM train t
-
 LEFT JOIN incident_train it ON t.id_train = it.id_train
-
 LEFT JOIN incident i ON it.id_incident = i.id_incident
-
 LEFT JOIN niveau_risque r ON i.id_niveau_risque = r.id_niveau_risque
-
 LEFT JOIN (
+	-- 
     SELECT m1.id_train, m1.heures_fonctionnement, m1.niveau_risque
 	FROM maintenance m1
 	INNER JOIN (
@@ -20,15 +25,11 @@ LEFT JOIN (
 		GROUP BY id_train
 	) m2 ON m1.id_train = m2.id_train AND m1.date_maintenance = m2.max_date
 ) m ON t.id_train = m.id_train
-
 GROUP BY t.id_train, t.heures_fonctionnement, m.niveau_risque, m.heures_fonctionnement
-
 HAVING 
-    (SUM(COALESCE(r.niveau, 0)) - COALESCE(m.niveau_risque, 0)) >= 3 
-    OR 
-    (t.heures_fonctionnement - COALESCE(m.heures_fonctionnement, 0)) >= 3000
-
-ORDER BY 
-    t.id_train;
+	(t.heures_fonctionnement - COALESCE(m.heures_fonctionnement, 0)) >= 3000
+	OR 
+	(SUM(COALESCE(r.niveau, 0)) - COALESCE(m.niveau_risque, 0)) >= 3 
+ORDER BY t.id_train;
 
 SELECT * FROM train_besoin_maintenance;
--- questions/q3/q3view.sql
+++ questions/q3/q3view.sql
@@ -1,3 +1,4 @@
+-- On récupère tous les départ de tous les trains pour chaque gare
 CREATE OR REPLACE VIEW depart_gare (id_gare, horaire_depart) AS
 SELECT t.gare_depart AS id_gare, h.horaire_depart FROM horaire_ligne h
 INNER JOIN trajet t ON h.id_trajet = t.id_trajet
@@ -5,6 +6,8 @@ ORDER BY t.gare_depart, h.horaire_depart;
 
 SELECT * FROM depart_gare;
 
+
+-- On récupère toutes les arrivées de tous les trains pour chaque gare
 CREATE OR REPLACE VIEW arrive_gare (id_gare, horaire_arrive) AS
 SELECT t.gare_arrive AS id_gare, h.horaire_arrive FROM horaire_ligne h
 INNER JOIN trajet t ON h.id_trajet = t.id_trajet
@@ -13,31 +16,29 @@ ORDER BY t.gare_arrive, h.horaire_arrive;
 SELECT * FROM arrive_gare;
 
 
---------------------
-
-
-SELECT COALESCE(d.id_gare,a.id_gare), d.horaire_depart, a.horaire_arrive FROM depart_gare d
-FULL OUTER JOIN arrive_gare a ON d.id_gare = a.id_gare AND d.horaire_depart = a.horaire_arrive
-ORDER BY  
-COALESCE(d.id_gare,a.id_gare),
-CASE
-	WHEN horaire_arrive IS NOT NULL AND horaire_depart IS NOT NULL THEN horaire_arrive
-	WHEN horaire_arrive IS NOT NULL THEN horaire_arrive
-	WHEN horaire_depart IS NOT NULL THEN horaire_depart
-	ELSE '00:00:00'
-END
-
-
--------------
-
-
+-- On calcule l'indice de surcharge prévu pour une horaire donnée pour chaque gare
+-- Pour chaque train qui part de la gare, son indice est réduit de 1
+-- Pour chaque train qui arrive dans la gare, son indice est augmenté de 1
+-- Cela signifie que l'indice peut être négatif, cependant, c'est normal
+-- L'indice indique la surcharge théorique de la gare, pas un nombre de train précis
+-- Les deux colones sont nécéssaire pour savoir si l'horaire est celle d'un train qui part ou qui arrive
+-- On obtient les deux colones grace au FULL OUTER JOIN et aux deux view précédente
+-- Ici, l'ordre des lignes est très important, car l'indice de surcharge est calculé ligne après ligne
 CREATE OR REPLACE VIEW gare_surcharge_prevue_tout_horaire (id_gare, horaire_debut, horaire_fin, surcharge) AS
 WITH horaire_gare (id_gare, horaire_depart, horaire_arrive) AS (
-	SELECT COALESCE(d.id_gare,a.id_gare), d.horaire_depart, a.horaire_arrive FROM depart_gare d
+	SELECT 
+	COALESCE(d.id_gare,a.id_gare) AS id_gare, 
+	d.horaire_depart, 
+	a.horaire_arrive 
+	FROM depart_gare d
 	FULL OUTER JOIN arrive_gare a ON d.id_gare = a.id_gare AND d.horaire_depart = a.horaire_arrive
 )
 SELECT 
-id_gare, horaire_depart, horaire_arrive,
+id_gare, 
+horaire_depart, 
+horaire_arrive,
+-- Pour chaque horaire dans chaque gare :
+-- On calcule la valeur de surcharge par rapport à toutes les valeurs précédentes
 SUM(
 	CASE
 		WHEN horaire_depart IS NULL THEN 1
@@ -45,13 +46,14 @@ SUM(
 		ELSE 0
 	END
 ) OVER (
-PARTITION BY id_gare ORDER BY (
-	CASE
-		WHEN horaire_arrive IS NOT NULL AND horaire_depart IS NOT NULL THEN horaire_arrive
-		WHEN horaire_arrive IS NOT NULL THEN horaire_arrive
-		WHEN horaire_depart IS NOT NULL THEN horaire_depart
-		ELSE '00:00:00'
-	END
+	PARTITION BY id_gare ORDER BY (
+		CASE
+			-- Le case nous permet de trier correctement les horaires sur les deux colones
+			WHEN horaire_arrive IS NOT NULL AND horaire_depart IS NOT NULL THEN horaire_arrive
+			WHEN horaire_arrive IS NOT NULL THEN horaire_arrive
+			WHEN horaire_depart IS NOT NULL THEN horaire_depart
+			ELSE '00:00:00'
+		END
 	)
 ) AS surcharge
 FROM horaire_gare
@@ -60,23 +62,29 @@ ORDER BY id_gare;
 SELECT * FROM gare_surcharge_prevue_tout_horaire
 
 
----------------
-
+-- La vue finale permettant de répondre à la question
+-- Utilise la vue "gare_surcharge_prevue_tout_horaire" 
+-- pour créer des intervales de temps et leur donner l'indice de surcharge
+-- On sélection ensuite seulement les intervales où:
+-- la valeur de surcharge >= 2
+-- l'horaire de fin de l'intervale est différente que celle de début
 CREATE OR REPLACE VIEW gare_surcharge_prevue (id_gare, horaire_debut, horaire_fin, surcharge) AS
 WITH intervales (id_gare, horaire_debut, horaire_fin, surcharge) AS (
-SELECT 
-id_gare, 
-COALESCE(horaire_debut,horaire_fin) AS horaire_debut,
-CASE 
-	WHEN lead(id_gare) OVER(ORDER BY(SELECT NULL)) = id_gare THEN 
-	COALESCE(
-		lead(horaire_debut) OVER(ORDER BY(SELECT NULL)),
-		lead(horaire_fin) OVER(ORDER BY(SELECT NULL))
-	)
-	ELSE NULL
-END AS horaire_fin,
-surcharge
-FROM gare_surcharge_prevue_tout_horaire
+	SELECT 
+	id_gare, 
+	-- Coalesce nous permet d'obtenir une horaire peut importe si l'un des colonnes est NULL
+	COALESCE(horaire_debut,horaire_fin) AS horaire_debut,
+	CASE 
+		-- On récupère l'horaire de la ligne suivante si elle appartient à la même gare
+		WHEN lead(id_gare) OVER(ORDER BY(SELECT NULL)) = id_gare THEN 
+			COALESCE(
+				lead(horaire_debut) OVER(ORDER BY(SELECT NULL)),
+				lead(horaire_fin) OVER(ORDER BY(SELECT NULL))
+			)
+		ELSE NULL
+	END AS horaire_fin,
+	surcharge
+	FROM gare_surcharge_prevue_tout_horaire
 )
 SELECT * FROM intervales
 WHERE horaire_debut != horaire_fin AND surcharge >= 2;
